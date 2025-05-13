36f7b7963bcc643663453f783289b480f643ee60
Humberto <humbertobrunolopes@gmail.com>
2025-05-13 00:06:57 -0300

    Fix Next.js Image Optimization

--- next.config.js
+++ next.config.js
@@ -4,19 +4,8 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'stackblitz.com',
-        port: '',
-        pathname: '/storage/blobs/**',
-      },
-    ],
-    domains: ['images.pexels.com'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  images: { 
+    unoptimized: true 
   },
   webpack: (config, { isServer }) => {
     config.cache = false;
